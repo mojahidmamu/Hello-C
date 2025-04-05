2239aded39e1c89e6e05f4a8f73d8eee25b24604
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-05 10:15:37 +0600

    addded new

--- first.c
+++ first.c
@@ -3,10 +3,24 @@
 int main()
 {
 
-    int age = 25;  // integer
+    int age = 25;       // integer
     float height = 5.9; // floating point number
-    char grade = 'A';// character
+    char grade = 'A';   // character
+
+    /**
+     * TOTAL KEYWORDS IN C LANGUAGE....... 
+auto        break       case        char
+const       continue    default     do
+double      else        enum        extern
+float       for         goto        if
+int         long        register    return
+short       signed      sizeof      static
+struct      switch      typedef     union
+unsigned    void        volatile    while
+
+     */
 
     printf("This is Mojahid if front of you");
+    printf("This is Mojahid\n");
     return 0;
 }
