5bcd8fa9115e938bbb166afe6a8b16b0a72edc46
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-05 09:54:32 +0600

    addded new

--- first.c
+++ first.c
@@ -1,6 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main () {
+int main()
+{
+
+    int age = 25;  // integer
+    float height = 5.9; // floating point number
+    char grade = 'A';// character
+
     printf("This is Mojahid if front of you");
     return 0;
 }
