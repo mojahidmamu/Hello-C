0aa24c22712f2c1b063e4ca4ed07b2ad77f65fe3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-08 21:36:09 +0600

    added

--- Module 3/mojahid.c
+++ Module 3/mojahid.c
@@ -1,5 +1,15 @@
-#include<stdio.h>
-int main () 
+#include <stdio.h>
+int main()
 {
+    int num;
+    scanf("%d", &num);
+    if (num > 250)
+    {
+        printf("Go to home");
+    }
+    else
+    {
+        printf("Go to market");
+    }
     return 0;
 }
