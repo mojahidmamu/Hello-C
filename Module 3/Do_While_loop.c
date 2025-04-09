9d027bc47772e2caf5dc02838c6f53d56a27d75b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-09 10:31:46 +0600

    added

--- Module 3/Do_While_loop.c
+++ Module 3/Do_While_loop.c
@@ -2,12 +2,10 @@
 int main()
 {
     int num = 1;
-
     do
     {
         printf("Number is: %d\n", num);
         num++;
     } while (num <= 5);
-
     return 0;
 }
--- Module 3/infinity.c
+++ Module 3/infinity.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
-int main(){
-   
+#include <stdio.h>
+int main()
+{
+    int i = -5;
+    while(i<3)
+    {
+    printf("hello ");
+    i+=2;
+    }
     return 0;
 }
