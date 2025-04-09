7cdef156482e5387abffdee34f57c41d10919e34
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-09 10:52:12 +0600

    added

--- Module 3/practice.c
+++ Module 3/practice.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-int main() {
-
-    return 0;
-}
+// #include<stdio.h>
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     if( n == 0)
+//     {
+//         printf("Zero");
+//     }
+//     else
+//     {
+//         printf("Non Zero");
+//     }
+//     return 0;
+// }
