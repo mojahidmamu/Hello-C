9230eef399a260fb7dc10db8e87c4edf47816d90
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-09 09:38:04 +0600

    added

--- Module 3/Do_While_loop.c
+++ Module 3/Do_While_loop.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int num = 1;
 
     do
     {
         printf("Number is: %d\n", num);
-        num++;  
+        num++;
     } while (num <= 5);
-    
-    
+
     return 0;
 }
