a54cb43d56bcad242af5c0f8579cdb9e47c5f31a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-09 08:14:18 +0600

    added

---
+++ Module 3/even_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i += 2)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d\n", i);
+        }
+        else
+        {
+            printf("%d\n", i);
+        }
+    }
+    
+    return 0;
+}
--- Module 3/loop.c
+++ Module 3/loop.c
@@ -16,7 +16,7 @@ int main()
     int sum = 0;
     for (int i = 1; i <= n; i++)
     {
-        sum = sum + i;
+        sum += i;
     }
     printf("%d\n", sum);
     return 0;
