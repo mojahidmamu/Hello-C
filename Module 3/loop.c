aac0e74e05a3e7453ef22ee67ea52e220cf66ca1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-09 07:28:52 +0600

    added

--- Module 3/loop.c
+++ Module 3/loop.c
@@ -1,6 +1,23 @@
+// For loop :
 #include <stdio.h>
 int main()
 {
-   
+    // for( int i = 1; i <= 10; i++){
+    //     printf("%d Sorry\n ", i);
+    // }
+
+    // for (int i = 1; i <= 5; i++) {
+    //     printf("%d\n", i);
+    // }
+
+    // Sum of 1 to N :
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    printf("%d\n", sum);
     return 0;
 }
