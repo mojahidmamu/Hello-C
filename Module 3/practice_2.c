fddafd8aba0af203ababc9272d4d87ce569b9936
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-10 09:34:46 +0600

    added new navbar and footer

--- Module 3/practice_2.c
+++ Module 3/practice_2.c
@@ -1,5 +1,51 @@
+// #include<stdio.h>
+// int main()
+// {
+//     printf("I Love Practice");
+//     return 0;
+// }
+
+// Problem-2
+// #include<stdio.h>
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("%d", a + b);
+//     return 0;
+// }
+
+// problem 3
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     long long int b;
+//     double c;
+//     char d;
+//     scanf("%d %lld %lf %c", &a, &b, &c, &d);
+//     printf("%d\n%lld\n%.2lf\n%c\n", a, b, c, d);
+//     return 0;
+// }
+
+// problem 4
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n); 
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("I Love Practice\n");
+//     }
+
+//     return 0;
+// }
+
+// problem 5
 #include<stdio.h>
 int main()
 {
+
     return 0;
 }
