a6dddebe8135a3e1e800356e0792c87c2a604aff
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-13 12:44:11 +0600

    added

---
+++ Module 5/capital_small_digit.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    scanf("%c", &x);
+    if ('0' >= x <= '9')
+    if (x >= '0' && x <= '9')
+    {
+       printf("IS DIGIT\n");
+    }
+    else {
+        printf("ALPHA\n");
+        if ('a' >= x <= 'z')
+        {
+          printf("IS SMALL");
+        }
+        else{
+            printf("IS CAPITAL");
+        }
+        
+    }
+    
+    return 0;
+}
