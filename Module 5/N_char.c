ce2811cab24a28985d276ef54f8ca9238e0635c0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-13 11:57:52 +0600

    added

--- Module 5/N_char.c
+++ Module 5/N_char.c
@@ -1,31 +1,31 @@
-// #include <stdio.h>
-// int main()
-// {
-//     char a;
-//     scanf("%c", &a);
-//     if(a >= 'a' && a >= 'z')
-//     {
-//         int ans = a - 32;
-//         printf("%c", ans);
-//     }
-//     else
-//     {
-//         int ans = a + 32;
-//         printf("%c", ans);
-//     }
-//     return 0;
-// }
-
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a , b;
-    scanf("%d %d", &a, &b);
-    if(a % b == 0 && b % a == 0){
-        printf("Multiples");
+    char a;
+    scanf("%c", &a);
+    if(a >= 'a' && a <= 'z')
+    {
+        int ans = a - 32;
+        printf("%c", ans);
     }
-    else {
-        printf("No Multiples");
+    else
+    {
+        int ans = a + 32;
+        printf("%c", ans);
     }
     return 0;
 }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a , b;
+//     scanf("%d %d", &a, &b);
+//     if(a % b == 0 && b % a == 0){
+//         printf("Multiples");
+//     }
+//     else {
+//         printf("No Multiples");
+//     }
+//     return 0;
+// }
