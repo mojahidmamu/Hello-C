37538e91a7e2ac563eb89460bb1590a93cc0a612
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-13 16:05:39 +0600

    added

--- Module 5/Difference.c
+++ Module 5/Difference.c
@@ -1,6 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    long long int A, B, C, D;
+    scanf("%lld %lld %lld %lld", &A, &B, &C, &D);
+    long long int X = (A * B) - (C * D);
+    printf("Difference = %lld\n", X);
     return 0;
 }
