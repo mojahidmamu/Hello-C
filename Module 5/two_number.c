24cfc6c8ce2ad2a713a9c3363fa9cd1876873ffd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-13 19:43:05 +0600

    added

--- Module 5/two_number.c
+++ Module 5/two_number.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("Floor %d\n");
+    printf("Ceil %d \n");
+    printf("Round %d\n");
     return 0;
 }
