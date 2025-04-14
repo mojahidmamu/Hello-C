2dd51fb0c8ad2e16620680fe7f41c2dd7b4dca33
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-14 07:38:01 +0600

    added

--- Module 6/loop_problem_solving.c
+++ Module 6/loop_problem_solving.c
@@ -1,6 +1,19 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 10;
+//     int b = a++;
+//     int c = ++b;
+//     printf("%d \n", c++);
+//     return 0;
+// }
+
 #include<stdio.h>
 int main()
 {
-    
+    int a = 10;
+    --a;
+    printf("%d\n", --a);
+    printf("%d", a);
     return 0;
 }
---
+++ Module 6/print_1_to_N.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    for(int i = 2; i <= N; i++){
+        if (i % 2 == 0)
+        {
+          printf("%d\n", i);
+        }
+    }
+    return 0;
+}
