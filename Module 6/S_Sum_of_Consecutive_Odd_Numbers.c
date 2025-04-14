212924daaeac3dc250f408a9e8cf24f7eb43d4cd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-14 21:53:42 +0600

    added

--- Module 6/S_Sum_of_Consecutive_Odd_Numbers.c
+++ Module 6/S_Sum_of_Consecutive_Odd_Numbers.c
@@ -1,6 +1,29 @@
-#include<stdio.h>
-int main()
-{
-    
+#include <stdio.h>
+
+int main() {
+    int T, X, Y;
+    scanf("%d", &T);
+
+    while (T--) {
+        scanf("%d %d", &X, &Y);
+
+        int sum = 0;
+        // Ensure X is less than Y
+        if (X > Y) {
+            int temp = X;
+            X = Y;
+            Y = temp;
+        }
+
+        // Iterate between X and Y (exclusive)
+        for (int i = X + 1; i < Y; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+
+        printf("%d\n", sum);
+    }
+
     return 0;
 }
