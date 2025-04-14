beb7f59a6164bc5f3dda3a5e7125315b00c15259
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-14 09:16:10 +0600

    added

--- Module 6/D_Fixed_Password.c
+++ Module 6/D_Fixed_Password.c
@@ -1,6 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int n;
+    while (scanf("%d", &n) != EOF)  
+    {
+        if (n == 1999)
+        {
+            printf("Correct\n");
+            break;
+        }
+        else
+        {
+            printf("Wrong\n");
+        }
+    }
     return 0;
 }
---
+++ Module 6/E_Max.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int a;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+         scanf("%d", &a);
+         printf("%d\n", a);
+    }
+    
+    return 0;
+}
