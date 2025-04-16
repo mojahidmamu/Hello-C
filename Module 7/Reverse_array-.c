6e8e81ac2bf06de61c1ed3df7674dbfa4628a835
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-16 09:12:30 +0600

    added

---
+++ Module 7/Reverse_array-.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]); 
+    }
+    for (int i = 5-1; i >= 0; i--)
+    {
+         printf("%d ", arr[i]);
+    }
+    return 0;
+}
---
+++ Module 7/array_initialization.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a; // only declare the variable
+    int b = 10; // declare and initialization variable
+    int ar[5];  // only declare the array
+    int arr[5] = {10,20,30,40,50}; // declare and initialization the array
+    return 0;
+}
