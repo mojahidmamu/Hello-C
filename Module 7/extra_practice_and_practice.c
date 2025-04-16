bb0580b225b9881bc85ee3d30c6024f1b8e1687c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-16 10:20:29 +0600

    added

--- Module 7/extra_practice_and_practice.c
+++ Module 7/extra_practice_and_practice.c
@@ -1,20 +1,55 @@
 // 1
-#include<stdio.h>
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[5];
+//     for (int i = 0; i < 5; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//         if (i % 2 == 0)
+//         {
+//             printf("%d ", arr[i]);
+//         }
+//     }
+
+//     return 0;
+// }
+
+// 2
+// #include<stdio.h>
+// int main()
+// {
+//     int arr[6];
+//     int count = 0;
+//     for (int i = 0; i < 6; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//         if (i % 2 != 1)
+//         {
+//             printf("%d ", arr[i]);
+//             count++;
+//         }
+//     }
+//     printf("\nTotal odd: %d", count);
+
+//     return 0;
+// }
+
+// 3
+#include <stdio.h>
 int main()
 {
-    int arr[5];
-    for (int i = 0; i < 5; i++)
+    int arr[6];
+    int max = 0;
+    for (int i = 0; i < 6; i++)
     {
         scanf("%d", &arr[i]);
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        if (i % 2 == 0)
+        if (i > max)
         {
-            printf("%d", arr[i]);
+            max = i;
         }
-        
     }
-    
+    printf("\nMax number is: %d", max);
+
     return 0;
 }
