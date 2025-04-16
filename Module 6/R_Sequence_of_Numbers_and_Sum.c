8348b4d5480bdb140bc9be6574a5d662ad7a1378
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-16 22:36:40 +0600

    added

--- Module 6/R_Sequence_of_Numbers_and_Sum.c
+++ Module 6/R_Sequence_of_Numbers_and_Sum.c
@@ -1,8 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int a, b;
     scanf("%d %d", &a, &b);
-    
+    int sum = 0;
+    if (a > b || b > a) 
+    {
+        for (int i = b; i <= a; i++)
+        {
+            printf("%d ", i);
+            sum += i;
+        }
+    }
+    else
+    {
+        printf("");
+    }
+    printf(" sum =%d", sum);
     return 0;
 }
