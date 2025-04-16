0b6de7d49941ec1f5b80045aa599d51ce293eaf7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-16 08:12:39 +0600

    added

--- Module 7/input_output.c
+++ Module 7/input_output.c
@@ -5,10 +5,26 @@ int main()
     for (int i = 0; i < 5; i++)
     {
         scanf("%d", &arr[i]);
+        // printf("%d ", arr[i]);
     }
     for (int i = 0; i < 5; i++)
     {
-        printf("%d\n", arr[i]);
+         printf("%d ", arr[i]);
     }
+    
+    
+    // scanf("%d %d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
+    // printf("%d %d %d %d %d", arr[0], arr[1], arr[2], arr[3], arr[4]);
+
+    // arr[1] = 12;
+    // printf("%d", arr[1]);
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     scanf("%d", &arr[i]);
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%d\n", arr[i]);
+    // }
     return 0;
 }
