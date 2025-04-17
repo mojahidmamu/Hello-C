9e1f8bda885203909f7151c7a08fe46a5692549f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-17 23:34:23 +0600

    added

--- Module 8/problem_5.c
+++ Module 8/problem_5.c
@@ -1,17 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
     scanf("%d", &n);
     int arr[n];
 
-    int X, v;
-    scanf("%d %d", &)
-
+   
     for (int i = 0; i < n; i++)
     {
-       scanf("%d", &arr[i]);
+        scanf("%d", &arr[i]);
     }
     
+    int X, v;
+    scanf("%d %d", &X, &v);
+
+
+    arr[X] = v;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
