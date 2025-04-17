e944e6ddffe9432622bba7022f622592304aecf6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-17 07:11:37 +0600

    added

--- Module 7/practice/A_Summation.c
+++ Module 7/practice/A_Summation.c
@@ -1,7 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int arr[4];
-    
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    long long int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+         long long int num;
+         scanf("%lld", &num);
+        sum += num;
+    }
+
+    printf("%lld", llabs(sum));
+
     return 0;
 }
