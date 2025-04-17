6964a73196bc320817ba3d2601b9f40c35108684
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-17 23:14:47 +0600

    added

--- Module 8/problem_3.c
+++ Module 8/problem_3.c
@@ -1,13 +1,17 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        /* code */
-    }
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (i % 2 != 0)
+//         {
+//              printf("")
+//         }
+        
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
--- Module 8/problem_4.c
+++ Module 8/problem_4.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 int main()
 {
+    int n;
+    scanf("%d", &n);
+    int sum_pos = 0;
+    int sum_neg = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+         scanf("%d", &val);
+         if (val > 0)
+         {
+            sum_pos += val;
+         }
+         else if (val < 0)
+         {
+             sum_neg += val;
+         }
+    }
+    printf("%d %d\n", sum_pos,sum_neg);
     
     return 0;
 }
