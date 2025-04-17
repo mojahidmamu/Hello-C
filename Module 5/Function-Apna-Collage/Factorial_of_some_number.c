3deb181ea67360c788b4705e70561e16b758a134
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-17 11:33:56 +0600

    added

--- Module 5/Function-Apna-Collage/Factorial_of_some_number.c
+++ Module 5/Function-Apna-Collage/Factorial_of_some_number.c
@@ -1,7 +1,20 @@
 // Function to Find the Factorial of a Number (using loop)
 #include<stdio.h>
+
+void factorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact*= i;
+    }
+    return fact;
+}
+
 int main()
 {
+    factorial(5);
+    factorial(3);
     
     return 0;
 }
