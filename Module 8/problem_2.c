57855dd5dab7e297b3aba326e8a7a657ab9b300f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-17 23:01:30 +0600

    added

--- Module 8/problem_2.c
+++ Module 8/problem_2.c
@@ -3,17 +3,19 @@ int main()
 {
     int N;
     scanf("%d", &N);
-    if (N < 0)
+    if (N > 0)
     {
-        for (int i = N; i<= 0; i++)
+        for (int i = 1; i <= N; i++)
         {
             printf("%d ", i);
         }
     }
-
-    for (int i = 1; i <= N; i++)
+    else
     {
-        printf("%d ", i);
+        for (int i = N; i <= 0; i++)
+        {
+            printf("%d ", i);
+        }
     }
 
     return 0;
