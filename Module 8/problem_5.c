abdd8011e0a95fa56054ee2f66bd47484d5444df
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-17 23:30:28 +0600

    added

--- Module 8/problem_5.c
+++ Module 8/problem_5.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 int main()
 {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    int X, v;
+    scanf("%d %d", &)
+
+    for (int i = 0; i < n; i++)
+    {
+       scanf("%d", &arr[i]);
+    }
     
     return 0;
 }
