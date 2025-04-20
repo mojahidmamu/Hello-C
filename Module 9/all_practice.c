556e52b267c27a4e23691ce2370554d36814ba3e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-20 21:37:54 +0600

    added

--- Module 9/all_practice.c
+++ Module 9/all_practice.c
@@ -27,7 +27,30 @@
 
 
 // remove an element from an arry:
-
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+         scanf("%d ", &array[i]);
+    }
+    int position;
+    scanf("%d ", &position);
+    // login
+    for (int i = position; i < n; i++)
+    {
+         array[i] = array[i = 1];
+    }
+    // pirnt
+    for (int i = 0; i < n-1; i++)
+    {
+         printf("%d ", array[i]);
+    }
+    return 0;
+}
 
 
 
