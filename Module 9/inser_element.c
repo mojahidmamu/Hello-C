ced0e642a3f9030d7716496b65365a597aa0cabd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-20 08:27:48 +0600

    added

--- Module 9/inser_element.c
+++ Module 9/inser_element.c
@@ -1,6 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int array[n + 1];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", &array[i]);
+    }
+    int index, value;
+    scanf("%d %d", &index, &value);
+
+    for (int i = n; i >= index + 1; i--)
+    {
+        array[i] = array[i - 1];
+    }
+    array[index] = value;
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d ", array[i]);
+    }
     return 0;
 }
