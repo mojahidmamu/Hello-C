6d9646306d5e7f73456db2bd807c69fd6702aa27
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-20 22:06:08 +0600

    added

--- Module 9/all_practice.c
+++ Module 9/all_practice.c
@@ -37,12 +37,12 @@
 //     }
 //     int position;
 //     scanf("%d ", &position);
-    // logic
+// logic
 //     for (int i = position; i <= n - 1; i++)
 //     {
 //         array[i] = array[i = 1];
 //     }
-    // pirnt 
+// pirnt
 //     for (int i = 0; i < n - 1; i++)
 //     {
 //         printf("%d ", array[i]);
@@ -50,19 +50,50 @@
 //     return 0;
 // }
 
+
+
+
 // reverse an array :
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int array[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d ", &array[n]);
+//     }
+//     int i = 0, j = n -1;
+//     while (i < j)
+//     {
+//         int temp = array[i];
+//         array[i] = array[j];
+//         array[j] = temp;
+//         i++;
+//         j--;
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", array[i]);
+//     }
+
+//     return 0;
+// }
+
+
 
 // swap two variables:
-#include <stdio.h>
-int main()
-{
-    int a = 10;
-    int b = 20;
-    int temp; 
-    temp = a;
-    a = b;
-    b = temp;
-    printf("%d %d %d", a, b, temp);
+// #include <stdio.h>
+// int main()
+// {
+//     int a = 10;
+//     int b = 20;
+//     int temp;
+//     temp = a;
+//     a = b;
+//     b = temp;
+//     printf("%d %d %d", a, b, temp);
 
-    return 0;
-}
+//     return 0;
+// }
