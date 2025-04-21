6d1795e7a7c2e18a2856fc666e74badd33c364f2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-21 07:42:24 +0600

    added

--- Module 10/practice_&_quiz.c
+++ Module 10/practice_&_quiz.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
-int main()
-{
-    char str[20];
-    scanf("%c", &str);
-    printf("%s", str);
-    return 0;
-}
+// #include<stdio.h>
+// int main()
+// {
+//     char str[20];
+//     scanf("%c", &str);
+//     printf("%s", str);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     char str[20];
+//     scanf("%s", &str);
+//     printf("%s", str);
+//     return 0;
+// }
+
