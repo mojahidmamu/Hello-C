70a16671dba14c5e6a8d8f02031db55eca140754
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-22 19:22:00 +0600

    added

--- Module 9/copy_2_array_in_another_array.c
+++ Module 9/copy_2_array_in_another_array.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
+    int n1;
+    scanf("%d", &n1);
+    int arr1[n1];
+    for (int i = 0; i < n1; i++)
+    {
+        scanf("%d ", &arr1[i]);
+    }
     
+    int n2;
+    scanf("%d", &n2);
+    int arr2[n2];
+    for (int i = 0; i < n2; i++)
+    {
+        scanf("%d ", &arr1[i]);
+    }
+
     return 0;
 }
