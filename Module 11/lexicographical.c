9b92f404c220fc6b32e2dfc0e0198ece915d1d0a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-23 15:27:07 +0600

    added

--- Module 11/lexicographical.c
+++ Module 11/lexicographical.c
@@ -1,10 +1,21 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main()
 {
     char a[101], b[101];
     scanf("%s %s", &a, &b);
-    
-    printf("%d",strcmp(a, b));
+    int val = strcmp(a, b);
+    if (val > 0)
+    {
+        printf("B IS smaller");
+    }
+    else if (val == 0)
+    {
+        printf(" all are equal");
+    }
+    else if (val < 0)
+    {
+        printf("A is smaller");
+    }
     return 0;
 }
