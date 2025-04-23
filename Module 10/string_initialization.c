b783098e4f499b8efc0b5f338cf597e448e470e3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-23 10:16:37 +0600

    added

--- Module 10/string_initialization.c
+++ Module 10/string_initialization.c
@@ -2,7 +2,9 @@
 int main()
 {
     // char a[5] = {'m', 'o', 'j', 'a'};]
-    char a[5] = "moja";
+    char a[5] = "moja"; // without space
+    char b[30] = "Abdullah all Mojahid"; // with space
+    printf("%s\n", b);
     printf("%s", a);
     return 0;
-}
+}  
