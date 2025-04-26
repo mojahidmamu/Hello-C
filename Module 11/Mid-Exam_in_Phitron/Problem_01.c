da1d28edf0bbe15e0969ca19fe3ebfb55e885ca4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-26 22:21:33 +0600

    added

--- Module 11/Mid-Exam_in_Phitron/Problem_01.c
+++ Module 11/Mid-Exam_in_Phitron/Problem_01.c
@@ -1,6 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int A[n];
+    int divide2 = 0, divide3 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", &A[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] % 2 == 0)
+        {
+            divide2++;
+        }
+        else if (A[i] % 3 == 0)
+        {
+            divide3++;
+        }
+    }
+
+    printf("%d %d\n", divide2, divide3);
     return 0;
 }
