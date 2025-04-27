7b55d19e05ccb923f5f5cbb8dcb6b61e139a5362
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-27 07:38:50 +0600

    added

--- Module 11/Mid-Exam_in_Phitron/Problem_02.c
+++ Module 11/Mid-Exam_in_Phitron/Problem_02.c
@@ -1,6 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    char a[100001];
+    scanf("%s ", a);
+    int consCount = 0;
+    for (int i = 0; a[i] != '\0'; i++)
+    {
+        if (a[i] != 'a' && a[i] != 'e' && a[i] != 'i' && a[i] != 'o' && a[i] != 'u')
+        {
+            consCount++;
+        }
+    }
+    printf("%d", consCount);
     return 0;
 }
