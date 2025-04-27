6791b1fdcec3618b3e0224b3f234dbf337cee569
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-27 08:29:13 +0600

    added

--- Module 11/Mid-Exam_in_Phitron/Problem_05.c
+++ Module 11/Mid-Exam_in_Phitron/Problem_05.c
@@ -1,6 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int testCase;
+    scanf("%d", &testCase);
+    for (int i = 0; i < testCase; i++)
+    {
+        int M1, M2, D;
+        scanf("%d %d %d", &M1, &M2, &D);
+
+        int allFarmers = M1 + M2;
+        int newWorkingDay = (M1 * D) / allFarmers;
+        int fewerDay = D - newWorkingDay;
+        
+        printf("%d\n", fewerDay);
+    }
+
     return 0;
 }
