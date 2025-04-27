ff63c20c53e183bcb8ecf429efa9fe36d400f1d0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-27 07:50:20 +0600

    added

--- Module 11/Mid-Exam_in_Phitron/Problem_03.c
+++ Module 11/Mid-Exam_in_Phitron/Problem_03.c
@@ -1,6 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int a;
+    scanf("%d ", &a);
+    for (int testCase = 0; testCase < a; testCase++)
+    {
+        char strng[10001];
+        scanf("%s", strng);
+
+        int capitalLeter = 0;
+        int smallLetter = 0;
+        int digitOrNumber = 0;
+
+        for (int i = 0; strng[i] != '\0'; i++)
+        {
+            if (strng[i] > 'A' && strng[i] < 'Z')
+            {
+                capitalLeter++;
+            }
+            else if (strng[i] > 'a' && strng[i] < 'z')
+            {
+                smallLetter++;
+            }
+            else if (strng[i] > '0' && strng[i] < '9')
+            {
+                digitOrNumber++;
+            }
+        }
+        printf("%d %d %d\n", capitalLeter, smallLetter, digitOrNumber);
+    }
+
     return 0;
 }
