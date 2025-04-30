192f5d5da9fa1f417d60b0b78fb6542958b004df
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-04-30 10:03:19 +0600

    added

---
+++ Module 6/F_Way_Too_Long_Words.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int testcase;
+    scanf("%d", &testcase);
+    char s[101];
+    for (int i = 0; i < testcase; i++)
+    {
+         scanf("%c", &s);
+         int len = strlen(s);
+    }
+    
+    
+    return 0;
+}
