d752d04b2dee12564f729affd2eb0a96bc3a3bd9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-01 21:36:31 +0600

    added

--- Module 9/practice.c
+++ Module 9/practice.c
@@ -110,3 +110,16 @@
 //     }
 //     return 0;
 // }
+
+// swap in two values: 
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int temp = a;
+    a= b;
+    b = temp;
+    printf("a is:%d & b is:%d", a, b);
+    return 0;
+}
