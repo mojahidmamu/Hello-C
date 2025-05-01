f0a3a147c303808b452f2eb188ba3be26896e57a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-01 20:13:44 +0600

    added

---
+++ Module 8/Fuction.c
@@ -0,0 +1,23 @@
+// #include<stdio.h>
+// void greet (){
+//     printf("Good Morning");
+// }
+// int main()
+// {
+//     greet();
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// void calculate (int number){
+//     int square = number * number;
+//     printf("Square of number: %d",square);
+// }
+// int main()
+// {
+    
+//     return 0;
+// }
+
