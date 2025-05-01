f07668590350bbb5ef86fc3dd71f6c02265add33
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-01 20:41:10 +0600

    added

--- Module 9/copy_2_array_in_another_array.c
+++ Module 9/copy_2_array_in_another_array.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+// #include <string.h>
 int main()
 {
+    // initialize & input the 1st array:
     int a;
     scanf("%d\n", &a);
     int arr1[a];
@@ -9,6 +11,7 @@ int main()
         scanf("%d ", &arr1[i]);
     }
 
+    // initialize & input the 2nd array:
     int b;
     scanf("%d\n", &b);
     int arr2[b];
@@ -17,7 +20,9 @@ int main()
         scanf("%d ", &arr1[i]);
     }
 
+    // initialize & input & some activities the 3rd array:
     int arr3[a + b];
+    // int len = strlen(arr3);
     for (int i = 0; i < a; i++)
     {
         arr3[i] = arr1[i];
@@ -27,7 +32,8 @@ int main()
         arr3[i + a] = arr2[i];
     }
 
-    for (int i = 0; i < a + b; i++)
+    // print the 3rd array:
+    for (int i = 0; i < a + b; i++) // len or a+b hobe...
     {
         printf("%d ", arr3[i]);
     }
