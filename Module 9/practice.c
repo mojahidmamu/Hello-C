5ba3b1853cb5e6b0c4e6518edca0d766ccbf41c3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-01 21:33:57 +0600

    added

--- Module 9/practice.c
+++ Module 9/practice.c
@@ -82,3 +82,31 @@
 //         printf("%d ", arr[i]);
 //     }
 // }
+
+
+// REVERSE an array: 
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int array[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d ", &array[i]);
+//     }
+//     int i = 0, j= n-1;
+//     while (i < j)
+//     {
+//         int temp = array[i];
+//         array[i] = array[j];
+//         array[j] = temp;
+//         i++;
+//         j--;
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", array[i]);
+//     }
+//     return 0;
+// }
