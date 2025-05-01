860e544b025b4c2ad2f0b3f4cb4cda22d5fe99d1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-01 21:39:59 +0600

    added

--- Module 9/practice.c
+++ Module 9/practice.c
@@ -112,14 +112,14 @@
 // }
 
 // swap in two values: 
-#include<stdio.h>
-int main()
-{
-    int a, b;
-    scanf("%d %d", &a, &b);
-    int temp = a;
-    a= b;
-    b = temp;
-    printf("a is:%d & b is:%d", a, b);
-    return 0;
-}
+// #include<stdio.h>
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     int temp = a;
+//     a= b;
+//     b = temp;
+//     printf("a is:%d & b is:%d", a, b);
+//     return 0;
+// }
