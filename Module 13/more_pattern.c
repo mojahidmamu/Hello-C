a20a0e36c465ad4355a64c5809f9707702583196
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-04 09:33:37 +0600

    added

--- Module 13/more_pattern.c
+++ Module 13/more_pattern.c
@@ -1,25 +1,49 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int star = 1;
+//     int space = n - 1;
+//     for (int i = 1; i <= n; i++) // for printing lines: 
+//     {
+//         for (int j = 1; j <= space; j++) // for printing space:
+//         {
+//             printf(" ");
+//         }
+//         for (int j = 1; j <= star; j++) // for printing star:
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//         star++;
+//         space--;
+//     }
+//     return 0;
+// }
+
+
+// more pattern:
 #include <stdio.h>
 int main()
 {
     int n;
     scanf("%d", &n);
-    int star = 1;
-    int space = n - 1;
-    for (int i = 1; i <= n; i++)
+    int star = n;
+    int space = 0;
+    for (int i = 1; i <= n; i++) // for printing lines:
     {
-        for (int j = 1; j <= space; j++)
+        for (int k = 1; k <= space; k++) // for printing space:
         {
             printf(" ");
         }
-
-        for (int j = 1; j <= star; j++)
+        for (int j = star; j >= 0; j--) // for printing star:
         {
             printf("*");
         }
         printf("\n");
-        star++;
-        space--;
+        star -= 2;
+        space++;
     }
-
     return 0;
 }
