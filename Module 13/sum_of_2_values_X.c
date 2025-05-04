6a62eb62ff56f7dcafacd51fdffc94c16657657d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-04 10:05:07 +0600

    added

---
+++ Module 13/sum_of_2_values_X.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int n; // array size
+    scanf("%d", &n);
+    int arr[n]; // initialize array
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", &arr[i]); // input the array
+    }
+    int x;           // initialize value of X..
+    scanf("%d", &x); // input the value of X...
+    int flag = 0;
+    for (int i = 0; i < n - 1; i++) // i from 0 to n-1,
+    {
+        for (int j = i + 1; j < n; j++) // j from i+1 to n
+        {
+            if (arr[i] + arr[j] == x) // if> i+j = x,
+            {
+                flag = 1; // flag is increment...
+            }
+        }
+    }
+    if (flag == 1)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+
+    return 0;
+}
