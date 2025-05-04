3950898077cb355e93cb148f59ea7df704657479
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-04 10:43:09 +0600

    added

--- Module 13/sorted_ascending_order.c
+++ Module 13/sorted_ascending_order.c
@@ -1,6 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
+    int n; // array size
+    scanf("%d", &n);
+    int arr[n]; // initialize array
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", &arr[i]); // input the array
+    }
+
+    for (int i = 0; i < n - 1; i++) // i from 0 to n-1,
+    {
+        for (int j = i + 1; j < n; j++) // j from i+1 to n...
+        {
+            if (arr[i] > arr[j]) // serial is not OK... 
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
     
     return 0;
 }
