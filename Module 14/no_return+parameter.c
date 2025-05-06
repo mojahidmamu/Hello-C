4702bcc23c4e9ca8f552d36a683c1bcf35403365
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-06 14:34:46 +0600

    added

--- Module 14/no_return+parameter.c
+++ Module 14/no_return+parameter.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+int sum(int a, int b)
+{
+    int ans = a + b;
+    printf("%d ", ans);
+}
 int main()
 {
-    
+    int a, b;
+    scanf("%d %d", &a, &b);
+    sum(a, b);
     return 0;
 }
