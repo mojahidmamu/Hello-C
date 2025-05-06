8da60cf47b8c18f66ab12908e3d081ae87c8fa6d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-06 15:08:14 +0600

    added

--- Module 14/variables_scope.c
+++ Module 14/variables_scope.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+int a= 10; // Global variable. Access in every function
+void sum() // void => for no return..
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int ans = a + b;
+    printf("%d ", ans);
+}
 int main()
 {
-    
+    sum();
     return 0;
 }
