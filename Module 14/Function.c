eb83d590602a4adc5809cf615524eb9335537019
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-06 09:08:17 +0600

    added

--- Module 14/Function.c
+++ Module 14/Function.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
-int main()
-{
-    
-    return 0;
-}
+// #include <stdio.h>
+// void double_it(int num)
+// {
+//     num * 2;
+//     return num;
+// }
+// int main()
+// {
+//     int a = 10;
+//     int double = double_it(a);
+//     printf("%d", double);
+//     return 0;
+// }
