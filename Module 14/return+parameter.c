4c22f6b5da1010b3da3f7dbaafbcf9fa86568002
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-06 14:14:54 +0600

    added

--- Module 14/return+parameter.c
+++ Module 14/return+parameter.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+int sum(int num1, int num2)
+{
+    int ans = num1 + num2;
+    return ans;
+}
 int main()
 {
-    
+    printf("%d\n", sum(10, 5     )); // print & function call in a line..
+    int mojahid = sum(10, 5);   // store in another variables..
+    printf("%d", mojahid);      // print in another line...
     return 0;
 }
