b431790e0106a9493168204cba299a84d6b6b8ad
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-08 08:39:15 +0600

    added

--- Module 15/check_pointer.c
+++ Module 15/check_pointer.c
@@ -3,6 +3,9 @@ int main()
 {
     int a = 10;
     printf("Value is: %d\n", a);
-    printf("Address is: %p", &a);
+    int *p = &a;
+    printf("Address is: %p\n", p);
+    *p = 100;
+    printf("Value is: %d ", a);
     return 0;
 }
