e3ed740a97c91ff36b000227f54f17178a0b8c1b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-08 08:46:01 +0600

    added

--- Module 15/pass_by_value.c
+++ Module 15/pass_by_value.c
@@ -1,14 +1,27 @@
-#include <stdio.h>
-void m(int *p)
+// #include <stdio.h>
+// void m(int *p)
+// {
+//     int i = 0;
+//     for (i = 0; i < 5; i++)
+//     {
+//         printf("%d ", p[i]);
+//     }
+// }
+// int main()
+// {
+//     int a[5] = {6, 5, 3};
+//     m(a);
+// }
+
+#include<stdio.h>
+void fun(int a)
 {
-    int i = 0;
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d ", p[i]);
-    }
+ printf("fun er a er address %p\n", &a);
 }
 int main()
 {
-    int a[5] = {6, 5, 3};
-    m(a);
+    int a = 10;
+    fun(a);
+    printf("main er a er value %p", &a);
+    return 0;
 }
