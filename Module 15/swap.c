00f65145970dc26c6f07c991d8ed7aabe4ea92b5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-08 07:22:04 +0600

    added

--- Module 15/swap.c
+++ Module 15/swap.c
@@ -11,3 +11,18 @@ int main()
     swap(&a, &b);
     printf("%d %d\n", a, b);
 }
+
+// #include<stdio.h>
+// void swap (int *p, int *q)
+// {
+//     int temp = *p;
+//     *p = *q;
+//     *q = temp;
+// }
+// int main()
+// {
+//     int a = 2, b=3;
+//     swap(&a, &b);
+//     printf("%d %d\n", a, b);
+//     return 0;
+// }
