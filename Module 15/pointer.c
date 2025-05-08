b61679498f3fec309ba9c017b251e66fb8001ed3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-08 06:50:16 +0600

    added

--- Module 15/pointer.c
+++ Module 15/pointer.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int a = 10;
+    printf("%d\n", a);
+    printf("%p\n", &a); // %p => for pointer variable..
+
+    int *ptr; // declare the pointer variable
+    ptr = &a; // &a for declare address of a..
+    *ptr = 200;
+    printf("%p\n", ptr);
+    printf("%d\n", a);
     return 0;
 }
