758e02685a8526bd9e633cf7c70e42d4331d2733
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-09 15:01:49 +0600

    added

--- Module 16/Assignment_03/Problem_02.c
+++ Module 16/Assignment_03/Problem_02.c
@@ -1,6 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int N;
+    scanf("%d", &N);
+    int space = N - 1;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = i; j >= 1; j--)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+        space--;
+    }
     return 0;
 }
+// Accepted
