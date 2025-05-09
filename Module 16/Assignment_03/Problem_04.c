bc69d94288acd3613e9808246d3dc68cea482a7f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-09 14:44:24 +0600

    added

--- Module 16/Assignment_03/Problem_04.c
+++ Module 16/Assignment_03/Problem_04.c
@@ -1,12 +1,33 @@
-#include<stdio.h>
-int is_palindrome(char S)
+#include <stdio.h>
+#include <string.h>
+int is_palindrome(char S[])
 {
-    
+    int len = strlen(S);
+    int i = 0, j = len - 1;
+
+    for (i = 0; i < len / 2; i++)
+    {
+        if (S[i] != S[len - i - 1])
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
 int main()
 {
     char S[1001];
     scanf("%s", S);
-    is_palindrome(S);
+    int result = is_palindrome(S);
+    if (result == 1)
+    {
+        printf("Palindrome\n");
+    }
+    else
+    {
+        printf("Not Palindrome\n");
+    }
+
     return 0;
 }
+// accepted
