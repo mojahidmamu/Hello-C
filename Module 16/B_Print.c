4f8e684dcc9c26670c10fde5c9c8913ea44b4aed
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-10 18:45:50 +0600

    added

--- Module 16/B_Print.c
+++ Module 16/B_Print.c
@@ -1,6 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+int print_1_to_n(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", i);
+    }
+}
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    print_1_to_n(n);
     return 0;
 }
