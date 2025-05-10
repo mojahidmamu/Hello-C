32a9e6c380e12f37f925de47790363c2998b9980
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-10 18:41:45 +0600

    added

--- Module 16/A_Add.c
+++ Module 16/A_Add.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+int sum(int a, int b)
+{
+    int summation = a+b;
+    printf("%d", summation);
+    return summation;
+}
 int main()
 {
-    
+    int a, b;
+    scanf("%d %d", &a, &b);
+    sum(a, b);
+
     return 0;
 }
