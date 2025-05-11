69b8ab8d3a7f9176a477434808fad14bafadbbc5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-11 07:07:15 +0600

    added

--- Module 16/G_Max_and_MIN.c
+++ Module 16/G_Max_and_MIN.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 int max_and_min(int ar[], int n)
 {
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", ar[i]);
+    }
     int max = ar[0], min = ar[0];
     for (int i = 0; i < n; i++)
     {
-        if ( ar[i] > max)
+        if (ar[i] > max)
         {
-            max = ar[i];
+            printf("%d", max = ar[i]);
         }
         if (min > ar[i])
         {
-            min = ar[i];
+            printf("%d", min = ar[i]);
         }
     }
-    printf("%d %d", min, max);
-    
+    // printf("%d %d", min, max);
 }
 int main()
 {
     int n;
     scanf("%d", &n);
     int ar[n];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d ", ar[i]);
-    }
 
     max_and_min(ar, n);
 
--- Module 16/Practice_Problem-1.c
+++ Module 16/Practice_Problem-1.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+#include <stdlib.h>
+int my_abs(int n)
+{
+    int absolate = abs(n);
+    // printf("%d", absolate);
+    return absolate;
+}
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int mojahid = my_abs(n);
+    printf("%d", mojahid);
     return 0;
 }
