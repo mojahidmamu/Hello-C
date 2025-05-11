613cd9bf1cb4cf90132cadc23b9c09d0ccfe4914
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-11 07:46:43 +0600

    added

--- Module 16/Practice_Problem_4.c
+++ Module 16/Practice_Problem_4.c
@@ -1,6 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+void change_it(int array[], int len)
+{
+
+    if (len > 0)
+    {
+        array[len - 1] = 100;
+    }
+}
 int main()
 {
+    int n;
+    scanf("%d", &n);
+    int array[1000];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+     
+    change_it(array, n);
+    // print updated array: 
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
     
     return 0;
 }
