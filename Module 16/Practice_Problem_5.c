0a8dacb3377f8f4105ab47663eb9ae553cc04e29
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-11 10:02:05 +0600

    added

--- Module 16/Practice_Problem_5.c
+++ Module 16/Practice_Problem_5.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
-void swap_it(int a, int b)
+void swp_it(int *a, int *b)
 {
-
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 int main()
 {
     int a, b;
     scanf("%d %d", &a, &b);
-    
+    swp_it(&a, &b);
+    printf("%d %d", a, b);
     return 0;
 }
