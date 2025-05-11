a16bbcf3b00891a338fcb9e42bacde8338140493
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-11 07:17:21 +0600

    added

--- Module 16/Practice_Problem-2.c
+++ Module 16/Practice_Problem-2.c
@@ -1,6 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+char my_len(char S[])
+{
+    int count = 0;
+    for (int i = 0;S[i] != '\0' ; i++)
+    {
+        count++;
+    }
+    
+    return count;
+    
+}
 int main()
 {
-    
+    char S[1000];
+    scanf("%s", &S);
+    int len = my_len(S);
+    printf("%d", len);
     return 0;
 }
