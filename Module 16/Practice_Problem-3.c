0ae75bac371a5e20d8a4022310ee381363674e29
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-11 07:27:03 +0600

    added

--- Module 16/Practice_Problem-3.c
+++ Module 16/Practice_Problem-3.c
@@ -1,6 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+int count_odd(int array[], int n)
+{
+    int odd = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            odd++;
+        }
+    }
+    return odd;
+}
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int array[1000];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    int count = count_odd(array, n);
+    printf("%d", count);
     return 0;
 }
