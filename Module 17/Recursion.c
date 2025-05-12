84ee5328f15ddb23bbd5efd36bd7b2fdeddda078
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-12 08:15:54 +0600

    added

--- Module 17/Recursion.c
+++ Module 17/Recursion.c
@@ -1,6 +1,25 @@
-#include<stdio.h>
+// #include<stdio.h>
+// void gello(){
+//     printf("Gello ");
+// }
+// void hello(){
+//     gello();
+//     printf("Hello ");
+// }
+// int main()
+// {
+//     printf("Main ");
+//     hello();
+//     return 0;
+// }
+
+#include <stdio.h>
+void fun()
+{
+    printf("fun\n");
+  
+}
 int main()
 {
-    
-    return 0;
+    fun();
 }
