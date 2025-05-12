8cc1c3ccdb45fe25c153d96d652167fc1988ab2e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-12 11:05:23 +0600

    added

--- Module 17/all_practice.c
+++ Module 17/all_practice.c
@@ -14,3 +14,23 @@
 //     hello(3);
 //     return 0;
 // }
+
+// #include<stdio.h>
+// void hello(int i)
+// {
+//     if (i <= 0)
+//     {
+//         return;
+//     }
+//     printf("%d\n", i);
+//     hello(i - 2);
+    
+// }
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     hello(n);
+//     return 0;
+// }
+
