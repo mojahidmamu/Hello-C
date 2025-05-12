fe79c54018f0c8b3019eef6cc682eac04b560a7b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-12 10:34:58 +0600

    added

--- Module 17/print_array_using_recursion.c
+++ Module 17/print_array_using_recursion.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
+void print_array(int arr[], int n, int i)
+{
+    if (i == n)
+    {
+        return;
+    }
+    
+    printf("%d\n", arr[i]);
+    print_array(arr, n, i+1);
+}
+
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", &array[i]);
+    }
+    print_array(array, n, 0);
     return 0;
 }
