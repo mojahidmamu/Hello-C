728df0a36220e68caf7046fed3938627ddfd27b2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-13 12:42:15 +0600

    added

---
+++ Module 18/K_Sum_Digits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char str[n+1];
+    scanf("%s", &str);
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += str[i]-'0';
+    }
+    printf("%d", sum);
+
+    return 0;
+}
