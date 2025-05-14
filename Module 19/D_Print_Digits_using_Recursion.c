d38f6718b71db1ab5aa895c7f1fec009531f7e80
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-14 17:55:27 +0600

    added

--- Module 19/D_Print_Digits_using_Recursion.c
+++ Module 19/D_Print_Digits_using_Recursion.c
@@ -1,8 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
+void rec(int n)
+{
+    if (n = 0) // base case
+    {
+        return;
+    }
+
+    rec(n / 10);
+    int last = n % 10;
+    printf("%d ", last);
+}
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    
+    int testCase;
+    scanf("%d", &testCase);
+    for (int i = 0; i < testCase; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        if (n == 0)
+        {
+            printf("0");
+        }
+        
+        rec(n);
+        printf("\n");
+    }
+
     return 0;
 }
--- Module 19/J_Factorial.c
+++ Module 19/J_Factorial.c
@@ -1,6 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+int factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    int mul = factorial(n - 1);
+    return n * mul;
+}
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int ans = factorial(n);
+    printf("%d", ans);
     return 0;
 }
