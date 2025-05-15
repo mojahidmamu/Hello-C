2134ad81a8d3260d6f55674ae142f13678b50c17
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-15 12:27:56 +0600

    added

--- Module 19/K_Max_Number.c
+++ Module 19/K_Max_Number.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-int max(int n)
+int max(int arr[], int n)
 {
+    if (n == 1)
+    {
+        return arr[0];
+    }
+    max(arr, n - 1);
+    return max;
 }
 int main()
 {
     int n;
     scanf("%d", &n);
-    int result = max(n);
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int result = max(arr, n);
     printf("%d", result);
     return 0;
 }
