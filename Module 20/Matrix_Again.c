f244dd3ea79420d8845e813433815b74ada9bcb6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-16 06:43:00 +0600

    added

--- Module 20/Matrix_Again.c
+++ Module 20/Matrix_Again.c
@@ -1,6 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int R, C;
+    scanf("%d %d", &R, &C);
+
+    int arr[R][C];
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            scanf("%d ", &arr[i][j]);
+        }
+    }
+
+    int last_row = R - 1;
+    for (int j = 0; j < C; j++)
+    {
+        printf("%d ", arr[last_row][j]);
+    }
+    printf("\n");
+
+    int last_col = C - 1;
+    for (int i = 0; i < R; i++)
+    {
+        printf("%d ", arr[i][last_col]);
+    }
+
     return 0;
 }
