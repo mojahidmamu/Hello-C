63e32929f6df624dbb594a9c7cc0445d612e15e5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-16 06:21:49 +0600

    added

--- Module 20/Find_Mising_Number.c
+++ Module 20/Find_Mising_Number.c
@@ -1,6 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++)
+    {
+        long long int M;
+        int A, B, C;
+        scanf("%lld %d %d %d", &M, &A, &B, &C);
+
+        long long int sum = A * B * C;
+        if (sum == 0)
+        {
+            if (M == 0)
+            {
+                printf("0\n");
+            }
+            else
+            {
+                printf("-1\n");
+            }
+        }
+        else if (M % sum != 0)
+        {
+            printf("-1\n");
+        }
+        else
+        {
+            printf("%lld\n", M / sum);
+        }
+    }
+
     return 0;
 }
+
+//  Accepted
--- Module 20/Jadu_Matrix.c
+++ Module 20/Jadu_Matrix.c
@@ -1,6 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
+    int R, C;
+    scanf("%d %d", &R, &C);
+
+    int arr[R][C];
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    
+    if (R == C)
+    {
+        
+    }
+    else{
+        printf("NO\n");
+    }
     
     return 0;
 }
