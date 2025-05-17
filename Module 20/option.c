c549bc7bc1102234594b02fec8915c945422e6bb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-17 12:20:15 +0600

    added

--- Module 20/option.c
+++ Module 20/option.c
@@ -1,6 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    int arr[n][n];
+    int jadu = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[i][j]);
+
+            if (i == j || i + j = n - 1)
+            {
+                if (arr[i][j] != 1)
+                {
+                    jadu = 0;
+                }
+            }
+            else
+            {
+                if (arr[i][j] != 0)
+                {
+                    jadu = 0;
+                }
+            }
+        }
+    }
+
+    if (jadu)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+
     return 0;
 }
