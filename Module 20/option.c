0e2e3da4b4558057a2d0d20e3a33d3562442f732
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-17 12:53:24 +0600

    added

--- Module 20/option.c
+++ Module 20/option.c
@@ -1,36 +1,98 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int R, C;
+//     scanf("%d %d", &R, &C);
+
+//     if (R != C)
+//     {
+//         printf("NO\n");
+//         return 0;
+//     }
+
+//     int arr[100][100];
+//     int jadu = 1;
+
+//     for (int i = 0; i < R; i++)
+//     {
+//         for (int j = 0; j < C; j++)
+//         {
+//             scanf("%d", &arr[i][j]);
+
+//             if (i == j || i + j == n - 1)
+//             {
+//                 if (arr[i][j] != 1)
+//                 {
+//                     jadu = 0;
+//                 }
+//             }
+//             else
+//             {
+//                 if (arr[i][j] != 0)
+//                 {
+//                     jadu = 0;
+//                 }
+//             }
+//         }
+//     }
+
+//     if (jadu)
+//     {
+//         printf("YES\n");
+//     }
+//     else
+//     {
+//         printf("NO\n");
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 int main()
 {
-    int n;
-    scanf("%d", &n);
+    int R, C;
+    scanf("%d %d", &R, &C);
 
-    int arr[n][n];
-    int jadu = 1;
-
-    for (int i = 0; i < n; i++)
+    if (R != C)
     {
-        for (int j = 0; j < n; j++)
+        printf("NO\n");
+        return 0;
+    }
+
+    int arr[100][100];
+    int flag = 1;
+
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
         {
             scanf("%d", &arr[i][j]);
+        }
+    }
 
-            if (i == j || i + j = n - 1)
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            if (i == j || i + j == R - 1)
             {
                 if (arr[i][j] != 1)
                 {
-                    jadu = 0;
+                    flag = 0;
                 }
             }
             else
             {
                 if (arr[i][j] != 0)
                 {
-                    jadu = 0;
+                    flag = 0;
                 }
             }
         }
     }
 
-    if (jadu)
+    if (flag)
     {
         printf("YES\n");
     }
