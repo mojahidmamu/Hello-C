08f7f9587d76634b5754b43de46a6be2de69dfa8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-17 12:54:04 +0600

    added

--- Module 20/Jadu_Matrix.c
+++ Module 20/Jadu_Matrix.c
@@ -1,10 +1,68 @@
+ // #include <stdio.h>
+// int main()
+// {
+//     int R, C;
+//     scanf("%d %d", &R, &C);
+
+//     if (R != C)
+//     {
+//         printf("NO\n");
+//         return 0;
+//     }
+
+//     int arr[100][100];
+//     int jadu = 1;
+
+//     for (int i = 0; i < R; i++)
+//     {
+//         for (int j = 0; j < C; j++)
+//         {
+//             scanf("%d", &arr[i][j]);
+
+//             if (i == j || i + j == n - 1)
+//             {
+//                 if (arr[i][j] != 1)
+//                 {
+//                     jadu = 0;
+//                 }
+//             }
+//             else
+//             {
+//                 if (arr[i][j] != 0)
+//                 {
+//                     jadu = 0;
+//                 }
+//             }
+//         }
+//     }
+
+//     if (jadu)
+//     {
+//         printf("YES\n");
+//     }
+//     else
+//     {
+//         printf("NO\n");
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 int main()
 {
     int R, C;
     scanf("%d %d", &R, &C);
 
-    int arr[R][C];
+    if (R != C)
+    {
+        printf("NO\n");
+        return 0;
+    }
+
+    int arr[100][100];
+    int flag = 1;
+
     for (int i = 0; i < R; i++)
     {
         for (int j = 0; j < C; j++)
@@ -13,32 +71,28 @@ int main()
         }
     }
 
-    int flag = 0;
-    if (R != C)
-    {
-        printf("NO\n"); // not squae..
-    }
-
     for (int i = 0; i < R; i++)
     {
         for (int j = 0; j < C; j++)
         {
             if (i == j || i + j == R - 1)
             {
-                if (arr[i][j] = 0)
+                if (arr[i][j] != 1)
                 {
-                    flag = 1;
-                    break;
+                    flag = 0;
                 }
             }
-            else if (arr[i][j] == 1)
+            else
             {
-                flag = 1;
-                break;
+                if (arr[i][j] != 0)
+                {
+                    flag = 0;
+                }
             }
         }
     }
-    if (flag == 1)
+
+    if (flag)
     {
         printf("YES\n");
     }
