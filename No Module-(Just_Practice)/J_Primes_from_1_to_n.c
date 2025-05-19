5a64e95c6a595f08a3a0545190105e06d3208efb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-19 09:08:34 +0600

    added

---
+++ No Module-(Just_Practice)/J_Primes_from_1_to_n.c
@@ -0,0 +1,15 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     for (int i = 2; i <= n; i++)
+//     {
+//         if (i % 1 == 0 && i % i == 0)
+//         {
+//             printf("%d ", i);
+//         }
+//     }
+
+//     return 0;
+// }
