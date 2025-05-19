08ff40a166f7c752c634b57bff02727b8bdf4370
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-19 11:19:18 +0600

    added

---
+++ No Module-(Just_Practice)/R_Permutation_with_arrays.c
@@ -0,0 +1,27 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int A[1000], B[1000];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &A[i]);
+//     }
+//     printf("\n");
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &B[i]);
+//     }
+
+//     if (A == B)
+//     {
+//         printf("yes\n");
+//     }
+//     else
+//     {
+//         printf("no\n");
+//     }
+
+//     return 0;
+// }
