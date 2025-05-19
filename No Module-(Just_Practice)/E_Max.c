bede276c6275db00c6d76bf843f5345c72389427
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-19 08:23:59 +0600

    added

---
+++ No Module-(Just_Practice)/E_Max.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    int max = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        scanf("%d", &a);
+        if (a > max)
+        {
+            max = a;
+        }
+    }
+    
+    printf("%d", max);
+    return 0;
+}
---
+++ No Module-(Just_Practice)/S_Sum_of_Consecutive_Odd_Numbers.c
@@ -0,0 +1,24 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     int a, b;
+//     int sum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d %d",&a, &b );
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (a, b % 2 != 0)
+//         {
+//             /* code */
+//         }
+           
+//     }
+    
+    
+//     return 0;
+// }
