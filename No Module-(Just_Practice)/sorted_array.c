d1afdeed55beb268193dac989144ece778eba21a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-20 08:26:05 +0600

    added

---
+++ No Module-(Just_Practice)/sorted_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int flag = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+
+    return 0;
+}
